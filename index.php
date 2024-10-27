eaf8e08550dc3dd8a80f7abcb8de924181ba7e2a
mejba13 <mejba.13@gmail.com>
2024-10-27 09:40:53 +0600

    deeply practice oop construct method and solve problem

--- index.php
+++ index.php
@@ -1,75 +1,28 @@
 <?php
 
+class MasteryOOPContructor {
 
-class Employee
-{
-    protected $name;
-    protected $salary;
-    public function __construct($name, $salary){
-        $this->name = $name;
-        $this->salary = $salary;
+    public $instituteName ;
+    public $StdID;
+    public $StdName;
+    public $DepartName;
+
+    public function __construct($SchoolName, $StdID, $StdName, $DepartName) {
+        $this->instituteName = $SchoolName;
+        $this->StdID = $StdID;
+        $this->StdName = $StdName;
+        $this->DepartName = $DepartName;
     }
 
-    public function getDetails()
-    {
-        echo "Name : " . $this->name . " Salary : " . $this->salary ."<br>";
+    public function studentDataSet() {
+        echo "The Student Data Set for $this->instituteName $this->StdID $this->StdName $this->DepartName" ."<br>";
     }
-
-    public function calculateBouns(){
-        echo "Bouns :".$this->salary*0.10 ."<br>";
-    }
-
-    public function FestivalBouns() {
-        echo "All employees will get Festival Bouns<br>";
-    }
-}
-
-class Manager extends Employee {
-    public function calculateBouns(){
-        parent::FestivalBouns();
-        echo "Bouns :". $this->salary*0.20 ."<br>";
-    }
-}
-
-class Developer extends Employee
-{
-    public $TourCost;
-
-    public function __construct($name, $salary, $tourCost)
-    {
-        parent::__construct($name, $salary);
-        $this->TourCost = $tourCost;
-    }
-    public function calculateBouns(){
-        echo "Bouns :". $this->salary*0.30 ."<br>";
-    }
-
-    public function getTourCost(){
-        echo "Tour Amount :".$this->TourCost."<br>";
-    }
-
 
 }
 
-class Designer extends Employee{
-    public function calculateBouns(){
-        echo "Bouns :". $this->salary*0.40."<br>";;
-    }
-}
-
-
-$Employee = new Employee("John Doe", "100.00");
-$Employee->getDetails();
-$Employee->calculateBouns();
-
-$Manager = new Manager("Mejba", "50.00");
-$Manager->getDetails();
-$Manager->calculateBouns();
-
-$Developer = new Developer("Maha", "100.00","50,000");
-$Developer->getDetails();
-$Developer->calculateBouns();
-$Developer->getTourCost();
-
-$Designer = new Designer('Martin','6000');
-$Designer->getDetails();
+$mejba = new MasteryOOPContructor('Daffodil International University','142154092','Engr Mejba Ahmed','Computer Science & Engineering');
+$mejba->studentDataSet();
+$maha = new MasteryOOPContructor('Averroes Internal School','MB24010','Maha Ahmed','English Medium');
+$maha->studentDataSet();
+$meskat = new MasteryOOPContructor('Dhaka International Univeristy','142154092','Meskat Ahmed','Computer Science & Engineering');
+$meskat->studentDataSet();
